c90e0ea6444fef659b12227f7e766f300ff59ee7
Smit Joshi <45530965+smit-joshi814@users.noreply.github.com>
2026-01-27 00:24:30 +0530

    update base url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: process.env.DOCUSAURUS_URL || "https://packages.smitjoshi.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.DOCUSAURUS_BASE_URL || "https://packages.smitjoshi.dev/spring-boot-starter-result/",
+  baseUrl: process.env.DOCUSAURUS_BASE_URL || "/spring-boot-starter-result/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
