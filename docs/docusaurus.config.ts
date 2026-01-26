4e99b5361162d3364717866eaafb0bb37032ddad
Smit Joshi <45530965+smit-joshi814@users.noreply.github.com>
2026-01-27 00:53:56 +0530

    comment out deployment branch in Docusaurus config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -26,7 +26,7 @@ const config: Config = {
   // organizationName: "homitra",
   // projectName: "spring-boot-starter-result",
 
-  deploymentBranch: "gh-pages",
+  // deploymentBranch: "gh-pages",
 
   onBrokenLinks: "warn",
 
